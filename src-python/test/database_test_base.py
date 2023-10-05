7590a7a8a116eb9c98d80702616af082ed6b8dd9
windlx <1418043337@qq.com>
2023-10-05 18:17:13 +0800

    [feature] better test

---
+++ src-python/test/database_test_base.py
@@ -0,0 +1,34 @@
+from os import remove
+
+from backend import app
+from model.resource_group import Base
+from service.database import get_db
+
+from pytest import fixture
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi.testclient import TestClient
+
+
+@fixture
+def context():
+    SQLALCHEMY_DATABASE_URL = "./data/fake_tag.db"
+    db = open(SQLALCHEMY_DATABASE_URL, 'w')
+    db.close()
+
+    engine = create_engine(f'sqlite:///{SQLALCHEMY_DATABASE_URL}',
+                           connect_args={"check_same_thread": False})
+    Base.metadata.create_all(engine)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    def override_get_db():
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    client = TestClient(app)
+    yield client
+    remove(SQLALCHEMY_DATABASE_URL)
