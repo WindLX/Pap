1e7057e8d365e94fa56da32db0622556db46a8e2
windlx <1418043337@qq.com>
2023-10-11 15:48:59 +0800

    [fix] pap.ps1

--- scripts/tool.ps1
+++ scripts/tool.ps1
@@ -1,8 +1,8 @@
 <#
 .Synopsis
-This PowerShell script is used for python type check, test the Pap backend, build the Pap frontend, pack the whole project.
+This PowerShell script is used for python type check, test the Pap backend, build the Pap frontend.
 .Description
-The tool.ps1 script is a versatile tool designed to simplify the process of Pap backend test, type check, build and pack. It offers the following functionality:
+The tool.ps1 script is a versatile tool designed to simplify the process of Pap backend test, type check and build. It offers the following functionality:
 
 - When executed without any options, it will DO NOTHING.
 
@@ -12,8 +12,6 @@ The tool.ps1 script is a versatile tool designed to simplify the process of Pap 
 
 - When the `-Build` option is provided, This script will build the frontend part
 
-- When the `-Pack` option is provided, This script will pack the whole project
-
 The script's flexibility allows it to adapt to your specific needs, whether you're focused on development or simply coding.
 .Parameter Check 
 Whether to run the test.
@@ -21,8 +19,6 @@ Whether to run the test.
 Whether to run the static type check.
 .Parameter Build
 Whether to build the frontend.
-.Parameter Pack
-Whether to pack the whole project.
 .Example
 To run the test:
 PS .\tool.ps1 -Test
@@ -32,9 +28,6 @@ PS .\tool.ps1 -Check
 
 To run the build:
 PS .\tool.ps1 -Build
-
-To run the pack:
-PS .\tool.ps1 -Pack
 #>
 
 [CmdletBinding()]
@@ -42,7 +35,6 @@ param (
     [switch] $Check,
     [switch] $Test,
     [switch] $Build,
-    [switch] $Pack,
 )
 
 # Function to install node modules
@@ -75,32 +67,9 @@ if ($Test) {
 } elseif ($Build) {
     Open-Node
     npm run build
-} elseif ($Pack) {
-    Open-Node
-    npm run build
-    if (Test-Path "PapPack") {
-        Remove-Item -Path .\PapPack -Recurse -Force
-    }
-    New-Item -Path .\PapPack -ItemType Directory
-    New-Item -Path .\PapPack\data -ItemType Directory
-    Copy-Item -Path .\data\config.toml -Destination .\PapPack\data\ -Recurse
-    Copy-Item -Path .\dist -Destination .\PapPack\dist -Recurse
-    Copy-Item -Path .\src-python -Destination .\PapPack\src-python -Recurse
-    Copy-Item -Path .\README.md -Destination .\PapPack\
-    Copy-Item -Path .\scripts\requirements_release.txt -Destination .\PapPack\
-    Copy-Item -Path .\scripts\pap.sh -Destination .\PapPack\
-    Copy-Item -Path .\scripts\pap.sh.man -Destination .\PapPack\
-    Remove-Item -Path .\PapPack\src-python\test -Recurse -Force
-    Set-ItemProperty -Path .\scripts\pap.sh -Name IsReadOnly -Value $false
-    Set-ItemProperty -Path .\scripts\pap.sh -Name Attributes -Value 'Normal'
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-    [System.IO.Compression.ZipFile]::CreateFromDirectory(".\PapPack", "PapPack.zip")
-    Remove-Item -Path .\PapPack -Recurse -Force
-    Write-Output "Pack Fin"
 } else {
     Write-Output "please choose a switch below neither this script will do nothing."
     Write-Output "1. -Check"
     Write-Output "2. -Test"
     Write-Output "3. -Build"
-    Write-Output "4. -Pack"
 }
